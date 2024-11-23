f31be973faf6d523c99b6c02c2d451b9ad52006e
giannisgkountras <gkountrasioannis@gmail.com>
2024-11-23 17:58:48 +0200

    Generate random pswd if none given in POST employee

--- src/models/Employee.js
+++ src/models/Employee.js
@@ -19,9 +19,17 @@ const Employee = {
 
     createEmployee: async (employeeData) => {
         const { fullName, email, role, warehouseID, password } = employeeData;
-
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
+        let hashedPassword;
+        if (!password) {
+            // Generate a random password
+            hashedPassword = await bcrypt.hash(
+                Math.random().toString(36).slice(-8),
+                10
+            );
+        } else {
+            // Hash the password
+            hashedPassword = await bcrypt.hash(password, 10);
+        }
 
         const [result] = await db.query(
             'INSERT INTO employee (fullName, email, role, warehouseID, password ) VALUES (?, ?, ?, ?, ?)',
